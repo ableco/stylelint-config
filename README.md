bdab76b735459bc9330055d66e1647e50fadf41b
cristianbgp <cristian.granda.pastor@gmail.com>
2020-09-10 15:24:47 -0500

    Add first version for @ableco/stylelint-config

---
+++ README.md
@@ -0,0 +1,29 @@
+# @ableco/stylelint-config
+
+This repository provides recommended Stylelint configuration for use on Able projects.
+
+## Installation
+
+### Using yarn:
+`yarn add @ableco/stylelint-config --dev`
+
+### Using npm:
+`npm install @ableco/stylelint-config --save-dev`
+
+## Usage
+
+In your `.stylelintrc` file, replace it with:
+
+```json
+{
+  "extends": "@ableco/stylelint-config"
+}
+```
+
+## Contributing
+
+Bug reports and pull requests are welcome on GitHub at https://github.com/ableco/stylelint-config.
+
+## License
+
+The library is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = {
+  rules: {
+    "at-rule-no-unknown": true,
+    "block-no-empty": true,
+    "color-no-invalid-hex": true,
+    "comment-no-empty": true,
+    "declaration-block-no-duplicate-properties": [
+      true,
+      {
+        ignore: ["consecutive-duplicates-with-different-values"],
+      },
+    ],
+    "declaration-block-no-shorthand-property-overrides": true,
+    "font-family-no-duplicate-names": true,
+    "font-family-no-missing-generic-family-keyword": true,
+    "function-calc-no-invalid": true,
+    "media-feature-name-no-unknown": true,
+    "no-descending-specificity": true,
+    "no-duplicate-selectors": true,
+    "no-empty-source": true,
+    "property-no-unknown": true,
+    "selector-pseudo-class-no-unknown": true,
+    "selector-pseudo-element-no-unknown": true,
+    "selector-type-no-unknown": true,
+    "unit-no-unknown": true,
+  },
+};
